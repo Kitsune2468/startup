e4722d1f4b3843ffd7881f6be0a0359177aeba16
Kitsune2468 <masefro@gmail.com>
2025-10-08 22:15:35 -0600

    React P1 - changed deployment script, updated some css/jsx

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -4,12 +4,12 @@ import './home.css';
 export function Home() {
   return (
     <main className="container-fluid bg-secondary text-center">
-        <div>
-          <h2>Your Rooms</h2>
-          <h3>(This section will show only when logged in.)</h3>
-          <ul>
-            <li><a href="room.html">PLACEHOLDER Room 1</a></li>
-            <li><a href="room.html">PLACEHOLDER Room 2</a></li>
+        <div className="home-page">
+          <h1 className="title">Your Rooms</h1>
+          <h3 className="subtitle">(This section will show only when logged in.)</h3>
+          <ul className="link-list">
+            <li className='room'><a href="room.html" className="room-link">PLACEHOLDER Room 1</a></li>
+            <li className='room'><a href="room.html" className="room-link">PLACEHOLDER Room 2</a></li>
           </ul>
         </div>
     </main>
