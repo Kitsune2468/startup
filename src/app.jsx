42b446f3ea6f26361d27ec2e0594e3f44f7ee506
Kitsune2468 <masefro@gmail.com>
2025-10-22 20:31:17 -0600

    React Pt2 - Login working. Took a minute to get login to look right.

--- src/app.jsx
+++ src/app.jsx
@@ -1,66 +1,77 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './app.css';
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import { Login } from './login/login';
+import { AuthState } from './login/authState';
 import { Room } from './room/room';
 import { Home } from './home/home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
 
 
 export default function App() {
-  return (
-    <BrowserRouter>
-        <div className="body bg-dark text-light">
-            <header className="container-fluid">
-                <nav className="navbar fixed-top navbar-dark">
-                    <NavLink className='nav-link navbar-brand' to="">Web-Tac-Toe</NavLink>
-                    <menu className="nav-menu">
-                        <li className="nav-item">
-                            <NavLink className='nav-link' to="">Login</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className='nav-link' to="home">Home</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className='nav-link' to="room">Room</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            TestUser
-                        </li>
-                    </menu>
-                </nav>
-            </header>
+    const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+    const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+    const [authState, setAuthState] = React.useState(currentAuthState);
 
-            <Routes>
-                <Route 
-                    path='/' 
-                    element={
-                        <Login 
-                            userName = {userName}
-                            authState = {authState}
-                            onAuthChange = {(userName, authState) => {
-                                setAuthState(authState);
-                                setUserName(userName);
-                            }}
-                        />
-                    } 
-                    exact 
-                />
-                <Route path='/home' element={<Home />} />
-                <Route path='/room' element={<Room />} />
-                <Route path='*' element={<NotFound />} />
-            </Routes>
+    return (
+        <BrowserRouter>
+            <div className="body bg-dark text-light">
+                <header className="container-fluid">
+                    <nav className="navbar fixed-top navbar-dark">
+                        <NavLink className='nav-link navbar-brand' to="">Web-Tac-Toe</NavLink>
+                        <menu className="nav-menu">
+                            <li className="nav-item">
+                                <NavLink className='nav-link' to="">Login</NavLink>
+                            </li>
+                            {authState === AuthState.Authenticated && (
+                                <li className="nav-item">
+                                    <NavLink className='nav-link' to="home">Home</NavLink>
+                                </li>
+                            )}
+                            {authState === AuthState.Authenticated && (
+                                <li className="nav-item">
+                                    <NavLink className='nav-link' to="room">Room</NavLink>
+                                </li>
+                            )}
+                            {authState === AuthState.Authenticated && (
+                                <li className="nav-item">
+                                    {userName}
+                                </li>
+                            )}
+                        </menu>
+                    </nav>
+                </header>
 
-            <footer className="bg-dark text-white-50">
-                <span className="text-reset">Author: Mason Fronce</span>
-                <a className="text-reset" href="https://github.com/Kitsune2468/startup">GitHub</a>
-                <a className="text-reset" href="https://github.com/webprogramming260/simon-css">Source</a>
-            </footer>
-        </div>
-    </BrowserRouter>
-  );
+                <Routes>
+                    <Route 
+                        path='/' 
+                        element={
+                            <Login 
+                                userName = {userName}
+                                authState = {authState}
+                                onAuthChange = {(userName, authState) => {
+                                    setAuthState(authState);
+                                    setUserName(userName);
+                                }}
+                            />
+                        } 
+                        exact 
+                    />
+                    <Route path='/home' element={<Home />} />
+                    <Route path='/room' element={<Room />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
+
+                <footer className="bg-dark text-white-50">
+                    <span className="text-reset">Author: Mason Fronce</span>
+                    <a className="text-reset" href="https://github.com/Kitsune2468/startup">GitHub</a>
+                    <a className="text-reset" href="https://github.com/webprogramming260/simon-css">Source</a>
+                </footer>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 function NotFound() {
-    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
+    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown. Auth Error.</main>;
 }
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import './login.css';
 
-export function Login() {
+import { Unauthenticated } from './unauthenticated';
+import { Authenticated } from './authenticated';
+import { AuthState } from './authState';
+
+export function Login({ userName, authState, onAuthChange }) {
   return (
-    <main className="container-fluid bg-secondary text-center login-form">
-        <h1 className="title">Welcome to Web-Tac-Toe!</h1>
-        <form method="get" action="index.html" className="login-info">
-            <div className="input-group mb-3">
-                <span className="input-group-text">@</span>
-                <input className="form-control" type="text" placeholder="your@email.com" />
-            </div>
-            <div className="input-group mb-3">
-                <span span className="input-group-text">🔒</span>
-                <input className="form-control" type="password" placeholder="password" />
-            </div>
-            <button type="submit" className="btn btn-primary login-button">Login</button>
-            <button type="submit" className="btn btn-secondary create-button">Create</button>
-        </form>
+    <main className="container-fluid bg-secondary text-center">
+        <div className="login-form">
+        {authState !== AuthState.Unknown && <h1 className="title">Welcome to Web-Tac-Toe!</h1>}
+        {authState === AuthState.Authenticated && (
+          <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+        )}
+        {authState === AuthState.Unauthenticated && (
+          <Unauthenticated
+            userName={userName}
+            onLogin={(loginUserName) => {
+              onAuthChange(loginUserName, AuthState.Authenticated);
+            }}
+          />
+        )}
+      </div>
     </main>
   );
 }
